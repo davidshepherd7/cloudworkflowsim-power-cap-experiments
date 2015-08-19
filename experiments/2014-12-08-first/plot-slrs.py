543dd784da3a33924b66300dc0dc0e681beaeca0
David Shepherd <davidshepherd7@gmail.com>
2015-08-19 15:01:39 +0100

    Plot makespan ratios instead of normalised makespans

--- experiments/2014-12-08-first/plot-slrs.py
+++ experiments/2014-12-08-first/plot-slrs.py
@@ -43,6 +43,10 @@ def main():
         data.append(data_dict)
 
 
+    data = sorted(data,
+                  key=lambda d: (d['application'], d['powerDipFraction'], d['algorithmName'], d['size']))
+
+
     # Plot data. Each application gets its own figure, each power cap
     # function gets its own marker type.
     for application, application_dataset in group_by(data, 'application'):
@@ -51,30 +55,50 @@ def main():
         colors = iter(['r', 'g', 'b', 'c', 'k', 'y'])
 
         fig, axes = plt.subplots(1,1)
-        fig.suptitle(application)
+        # fig.suptitle(application)
         axes.set_xlabel("Number of nodes in DAG")
-        axes.set_ylabel("Normalised makespan")
+        axes.set_ylabel("Makespan ratio")
 
 
         for power_dip_fraction, dataset_1 in group_by(application_dataset, 'powerDipFraction'):
+
+            output_data = {}
 
             for algorithm, dataset in group_by(dataset_1, 'algorithmName'):
 
                 sizes = []
                 means = []
                 for size, a in group_by(dataset, 'size'):
-                    means.append(sp.mean([d['makespan']/ d['optimalMakespan'] for d in a]))
-                    sizes.append(size)
+                    means.append(sp.mean([d['makespan'] for d in a]))
+                    sizes.append(int(size))
 
-                axes.scatter(sizes, means, marker=next(markers),
-                             color=next(colors), label=str(power_dip_fraction) +
-                             ' ' + algorithm)
+                output_data[algorithm] = (means, sizes)
+
+            main_algo = "HEFT-like"
+            relative_algo = "FCFS-like"
+
+            if output_data[relative_algo][1] != output_data[main_algo][1]:
+                print("Some sizes missing for one of the algorithms, got",
+                      output_data[relative_algo][1], output_data[main_algo][1])
+                continue
+
+            makespan_ratios = sp.array(output_data[main_algo][0]) /  sp.array(output_data[relative_algo][0])
+            sizes = output_data[main_algo][1]
+
+
+            axes.scatter(sizes, makespan_ratios, marker=next(markers),
+                         color=next(colors)) # label=str(power_dip_fraction) if npower_functions != 1 else None)
+
 
         axes.legend(loc=0)
-        axes.set_ylim([1.0, 2.0])
+        axes.set_ylim([0.5, 1.0])
+        axes.set_xlim([0, 1100])
 
         if args.save_as is not None:
-            fig.savefig(args.save_as + "-" + application + ".pdf")
+            fig.savefig(args.save_as + "-" + application + ".pdf",
+                        bbox_inches='tight',
+                        pad_inches=0.0,
+                        transparent=True)
 
     plt.show()
 
